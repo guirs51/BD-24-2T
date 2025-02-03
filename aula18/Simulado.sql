834b5b7cc2e8547f725139383514d4f3677aa7cb
Guilherme Dias Villanova <143645273+guirs51@users.noreply.github.com>
2025-02-03 17:27:13 -0300

    Add files via upload

---
+++ aula18/Simulado.sql
@@ -0,0 +1,95 @@
+CREATE DATABASE hogwarts 
+
+CREATE TABLE casa (
+    IDcasa INT NOT NULL PRIMARY KEY
+    nomeDaCasa VARCHAR (20)
+)
+
+CREATE TABLE Feitico (
+    IDFeitico INT NOT NULL PRIMARY KEY
+    NomeFeitico VARCHAR (20)
+)
+
+CREATE TABLE Materia (
+    IDMateria INT NOT NULL PRIMARY KEY
+    nomeMateria VARCHAR (20)
+    IDFeitico INT NOT NULL
+    CONSTRAINT fk_feitico (IDFeitico) REFERENCES Feitico (IDFeitico)
+)
+
+CREATE TABLE professores (
+    IDPROFESSOR INT NOT NULL PRIMARY KEY  
+    nomeProfessor VARCHAR (80)
+    idadeProfessor INT
+    IDMateria INT NOT NULL
+    CONSTRAINT fk_materia (IDMateria) REFERENCES Materia (IDMateria)
+    IDcasa INT NOT NULL
+    CONSTRAINT fk_casa (IDcasa) REFERENCES casa (IDcasa)
+    IDFeitico INT NOT NULL
+    CONSTRAINT fk_feitico (IDFeitico) REFERENCES Feitico (IDFeitico)
+)
+
+CREATE TABLE personagem (
+    idPersonagem INT NOT NULL PRIMARY KEY 
+    nomePersonagem varchar (80)
+    dataDeNascimento varchar (100)
+    IDcasa INT NOT NULL
+    CONSTRAINT fk_casa (IDcasa) REFERENCES  casa (IDcasa)
+    IDMateria INT NOT NULL
+    CONSTRAINT fk_materia (IDMateria) REFERENCES Materia (IDMateria)
+    IDFeitico INT NOT NULL
+    CONSTRAINT fk_feitico (IDFeitico) REFERENCES Feitico (IDFeitico)
+    IDPROFESSOR INT NOT NULL
+    CONSTRAINT fk_professor (IDPROFESSOR) REFERENCES professores (IDPROFESSOR)
+)
+
+INSERT INTO casa (IDcasa, nomeDaCasa)
+VALUES
+(1, "Grifinória");
+(2, " Corvinal");
+(3, "Lufa-Lufa");
+(4, "Sonserina")
+
+INSERT INTO Feitico (IDFeitico, NomeFeitico)
+VALUES
+(1,"Lumos");
+(2, "Maldição Imperius");
+(3, "Transfiguração");
+(4, "Maldição da Morte")
+
+INSERT INTO Materia (IDMateria, nomeMateria, IDFeitico)
+VALUES
+(1, "Defesa Contra as Artes das Trevas", 4);
+(2, "Transfiguração", 3);
+(3, "Trato das Criaturas Mágicas", 2);
+(4, "Herbologia", 1)
+
+
+INSERT INTO (IDPROFESSOR, nomeProfessor, idadeProfessor,IDMateria,IDcasa,IDFeitico)
+VALUES
+(1, "Dumbledore", 115, 2,1,3);
+(2,"Severo Snape", 38, 1, 4,4);
+(3,"Silvano Kettleburn",56,3,3,2);
+(4,"Neville Longbottom", 30, 4,1,1)
+
+INSERT INTO personagem (idPersonagem,nomePersonagem,dataDeNascimento,IDMateria, IDcasa, IDFeitico)
+VALUES
+(1,"Harray potter", "31 de julho de 1980", 1,1,1)
+(2, "Draco Lúcio Malfoy", "5 de junho de 1980",1,4,4)
+(3,"Hermione Granger", "19 de setembro de 1979", 3,1,3)
+(4,"Ron Weasley", "1 de março de 1980", 4,1,2)
+(5,"Voldemort", "31 de dezembro de 1926", 1,4,4)
+
+SELECT nomePersonagem, nomeDaCasa
+FROM  personagem, casa
+
+SELECT  * FROM personagem 
+WHERE IDcasa = 1
+
+CREATE OR REPLACE VIEW  view_personagens_grifinoria AS 
+SELECT nomePersonagem, dataDeNascimento , nomeDaCasa
+FROM personagem
+INNER JOIN casa
+ON personagem.IDcasa = casa.IDcasa
+
+CREATE INDEX nomePersonagem ON  (idPersonagem) 
